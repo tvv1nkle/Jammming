a5939c63f0162ee609c8e6c3b002d9c024418646
Nutchawakorn NaLampang <nutchawakorn.n@gmail.com>
2022-07-21 13:13:36 +0700

    renderAction

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -4,8 +4,26 @@ import React, { Component } from "react";
 import './Track.css';
 
 class Track extends Component {
+
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button>-</button>
+    } else {
+    }
+  }
+
+
+
   render() {
-    return <div>Track</div>;
+    return (
+      <div class="Track">
+        <div class="Track-information">
+          <h3>track name</h3>
+          <p>track artist | track album </p>
+        </div>
+        <button class="Track-action"></button>
+      </div>
+    );
   }
 }
 
