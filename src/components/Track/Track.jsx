b8196236e01c8e27ae9db2b85bdc0fa3a71490f6
Nutchawakorn NaLampang <nutchawakorn.n@gmail.com>
2022-07-21 13:03:18 +0700

    delete

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -5,15 +5,7 @@ import './Track.css';
 
 class Track extends Component {
   render() {
-    return (
-      <div class="Track">
-        <div class="Track-information">
-          <h3>track name</h3>
-          <p>track artist | track album </p>
-        </div>
-        <button class="Track-action"><!-- + or - will go here --></button>
-      </div>
-    )
+    return <div>Track</div>;
   }
 }
 
